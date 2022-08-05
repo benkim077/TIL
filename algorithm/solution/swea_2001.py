296737ca21f20e91a9c97d613491580adacbc5bd
benkim077 <123vfcx@gmail.com>
2022-08-05 17:54:02 +0900

    2001

--- algorithm/solution/swea_2001.py
+++ algorithm/solution/swea_2001.py
@@ -1,26 +1,23 @@
-my_list = [[1, 3, 3, 6, 7], [8, 13, 9, 12, 8], [4, 16, 11, 12, 6], [2, 4, 1, 23, 2], [9, 13, 4, 7, 3]]
-
 t = int(input())
-n, m = map(int, input().split())
 
 for i in range(1, t+1):
     my_list = []
+    n, m = map(int, input().split())
     for _ in range(n):
         my_list.append(list(map(int, input().split())))
 
-rlt = 0
-for k in range(len(my_list) - m + 1):
-    for j in range(len(my_list) - m + 1):
-        temp = 0
-        # 계산
-        for x in range(m):
-            for y in range(m):
-                temp += my_list[x+k][y+j]
-        print('<<<', temp)
+    rlt = 0
+    for k in range(len(my_list) - m + 1):
+        for j in range(len(my_list) - m + 1):
+            temp = 0
+            # 계산
+            for x in range(m):
+                for y in range(m):
+                    temp += my_list[x+k][y+j]
 
-        if rlt >= temp:
-            pass
-        else:
-            rlt = temp
+            if rlt >= temp:
+                pass
+            else:
+                rlt = temp
+    print('<<<', temp)
 
-print(f'#{i} {rlt}')
