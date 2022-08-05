cd6f37182db47882dcaa603019d8438dbb45196d
benkim077 <123vfcx@gmail.com>
2022-08-05 18:22:51 +0900

    2001

--- algorithm/solution/swea_2001.py
+++ algorithm/solution/swea_2001.py
@@ -1,23 +1,37 @@
-t = int(input())
+import sys
 
-for i in range(1, t+1):
+sys.stdin = open(r"input.txt","r")
+
+T = int(input())
+
+for t in range(1, T+1):
     my_list = []
     n, m = map(int, input().split())
     for _ in range(n):
         my_list.append(list(map(int, input().split())))
 
     rlt = 0
-    for k in range(len(my_list) - m + 1):
-        for j in range(len(my_list) - m + 1):
+    # 이동
+    for k in range(n - m + 1):
+        for j in range(n - m + 1):
             temp = 0
-            # 계산
+            # 내려침
             for x in range(m):
                 for y in range(m):
                     temp += my_list[x+k][y+j]
 
+            # 비교
             if rlt >= temp:
                 pass
             else:
                 rlt = temp
-    print('<<<', temp)
 
+    print(f'#{t} {rlt}')
+# test case 49
+# 1
+# 5 2
+# 1 3 3 6 7
+# 8 13 9 12 8
+# 4 16 11 12 6
+# 2 4 1 23 2
+# 9 13 4 7 3
