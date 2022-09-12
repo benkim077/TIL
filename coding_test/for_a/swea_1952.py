67ebcf4f401a90c08968a0efb07ac7b0f140bcbc
benkim <123vfcx@gmail.com>
2022-09-12 23:59:42 +0900

    0912

--- coding_test/for_a/swea_1952.py
+++ coding_test/for_a/swea_1952.py
@@ -7,16 +7,14 @@ for tc in range(1, T + 1):
     day_p, month_p, three_p, year_p = map(int, input().split())
     data = [0] + list(map(int, input().split()))
 
-    print(data)
-
     ans_lst = [0] * 13
-    for i in range(1, 12):
+    for i in range(1, 13):
         ans_lst[i] = min(data[i] * day_p, month_p) + ans_lst[i - 1]
 
         if i > 2:
             ans_lst[i] = min(ans_lst[i], three_p + ans_lst[i - 3])
     ans = min(ans_lst[-1], year_p)
 
-    print(f'{tc} {ans}')
+    print(f'#{tc} {ans}')
     
     
